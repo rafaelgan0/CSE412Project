07a76a2bd0ea3a98e4e6d5e1bcdc61cdd8fcec59
rafaelgan0 <105383204+rafaelgan0@users.noreply.github.com>
2023-11-22 15:21:26 -0700

    modified app

--- focusapp/src/App.js
+++ focusapp/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get("http://127.0.0.1:5000/api/data")
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Your React App</h1>
+            <ul>
+                {data.map(user => (
+                    <li key={user.UserID}>
+                        <strong>User ID:</strong> {user.UserID}<br />
+                        <strong>Password:</strong> {user.Password}<br />
+                        <strong>Birthday:</strong> {user.Birthday}<br />
+                        <strong>Bio:</strong> {user.Bio}<br />
+                        <strong>Profile Image URL:</strong> {user.ProfileImageUrl}
+                        <hr />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
