426adc23732e89148652a01c9655a3dfb2578e62
rafaelgan0 <105383204+rafaelgan0@users.noreply.github.com>
2023-11-22 15:20:32 -0700

    db file

---
+++ focusapp/src/db/db.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import psycopg2
+import os
+
+connection_string = os.environ.get('CONNECTION_STRING')
+
+app = Flask(__name__)
+CORS(app, origins="*")
+
+@app.route('/api/data')
+def get_data():
+    # Connect to your PostgreSQL database
+    connection = psycopg2.connect(connection_string)
+    
+    print("Connected to the database successfully!")
+
+    # Create a cursor object
+    cursor = connection.cursor()
+
+    # Execute a query
+    cursor.execute("SELECT * FROM \"User\"")
+
+    # Fetch the results
+    data = cursor.fetchall()
+
+    user_data = [
+        {
+            "UserID": record[0],
+            "Password": record[1],
+            "Birthday": record[2],
+            "Bio": record[3],
+            "ProfileImageUrl": record[4]
+        }
+        for record in data
+    ]
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+    return jsonify(user_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
